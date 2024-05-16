4a96dd5cebcf80d47dc37e026a70586294fcc801
San <jocksan.brittos@gmail.com>
2024-05-16 11:25:14 +0200

    Configurando pastas e organizacao de code

--- bootstrap.php
+++ bootstrap.php
@@ -3,3 +3,4 @@
 require __DIR__ . '/vendor/autoload.php';
 
 new San\Framework\Router;
+new App\Module;
--- src/Router.php
+++ src/Router.php
@@ -4,4 +4,8 @@ namespace San\Framework;
 
 class Router
 {
+    public function __construct()
+    {
+        echo '<h1>Olá Projeto, vem em mim fi!</h1>';
+    }
 }
