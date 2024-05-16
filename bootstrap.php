0461cfc7422310243556e900921d8d5f8688befe
San <jocksan.brittos@gmail.com>
2024-05-16 13:03:38 +0200

    Configurando rotas dinamicas

--- bootstrap.php
+++ bootstrap.php
@@ -7,8 +7,8 @@ $router = new San\Framework\Router;
 $router->add('/',  function () {
     return 'Estamos na homepage';
 });
-$router->add('/projects',  function () {
-    return 'Estamos Listando Projetos';
+$router->add('/projects/(\d+)',  function ($params) {
+    return 'Estamos Listando o projeto de id: ' . $params[1];
 });
 
 echo $router->run();
