e78789749be5a8b0704004c5279b20236a8203b7
San <jocksan.brittos@gmail.com>
2024-05-17 14:07:50 +0200

    Separando rotas usando o Pimple

---
+++ config/containers.php
@@ -0,0 +1,20 @@
+<?php
+
+use Pimple\Container;
+
+
+$container = new Container();
+
+$container['db'] = function () {
+    $dsn = 'mysql:host=localhost; dbname=pp_project_manager';
+    $username = 'root';
+    $password = null;
+    $options = [
+        \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAME utf8'
+    ];
+
+    $pdo = new \PDO($dsn, $username, $password, $options);
+
+    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+};
